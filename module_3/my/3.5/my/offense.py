2c3e9aa3f8266aaf54ff6f5c3ff88261ac9e4805
alexhead <alexhead4@gmail.com>
2020-04-18 13:54:31 +0500

    %18.04.2020%

--- module_3/my/3.5/my/offense.py
+++ module_3/my/3.5/my/offense.py
@@ -1,13 +1,9 @@
 import csv
-import re
-import pandas as pd
-from pathlib import Path
 
-def read_them_all(files, **kwargs):
-    return pd.concat([pd.read_csv(f, **kwargs) for f in files], ignore_index=True)
-
-p = Path(r'../files')
-
-phones = [2383482]
-df = read_them_all(p.glob('Crimes.csv'))
-print(df.query("A in @phones"))
+with open("../files/Crimes.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+       for i in range(row):
+          if i == 0:
+              print(row[i])
+       print(row)
