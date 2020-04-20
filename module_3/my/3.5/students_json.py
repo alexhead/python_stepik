f9950cd21951ac309d2db11065328889fe41754f
alexhead <alexhead4@gmail.com>
2020-04-20 17:57:55 +0500

    %20.04.2020%

--- module_3/my/3.5/students_json.py
+++ module_3/my/3.5/students_json.py
@@ -12,17 +12,21 @@ studens_2 = {
    "first_name": "Wirt",
     "fast_name": "Wood",
     "certificate": True,
-    "scores": [70, 80, 90],
+    "scores": [70, 80.2, 90],
     "description": "Nicelly done"
   }
 
-Data = [studens_1, studens_2]
+#Data = [studens_1, studens_2]
 
-data_json = json.dumps(Data, indent=4, sort_keys=True)
-data_again = json.loads(data_json)
-print(sum(data_again[0]))
+#data_json = json.dumps(Data, indent=4, sort_keys=True)
+#data_again = json.loads(data_json)
+#print(sum(data_again[0]["scores"]))
 
 #print(json.dumps(Data, indent=4, sort_keys=True))
 
 #with open("st.json", "w") as f:
 #   json.dump(Data, f, indent=4, sort_keys=True)
+
+with open("st.json", "r") as f:
+    data_again = json.load(f)
+    print(sum(data_again[1]["scores"]))
